7b4258fd0a3401314bc7a6f0d0e53b809e7ee829
bhargav <bhargav.mech12@gmail.com>
2020-06-16 16:33:22 +0530

    ViewsCreated

--- MyApp/views.py
+++ MyApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def HomeView(request):
+    return render(request,'MyTemp/home.html')
+def IndexView(request):
+    return render(request,'MyTemp/Index.html')
